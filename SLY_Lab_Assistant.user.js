8a2558eb9092383a590cb70ede2a45326aeb95fe
jonswallker <jonswallker@gmail.com>
2026-01-02 20:55:21 +0200

    Update SLY_Lab_Assistant.user.js

--- SLY_Lab_Assistant.user.js
+++ SLY_Lab_Assistant.user.js
@@ -20,7 +20,7 @@
 
     let enableAssistant = false;
     let initComplete = false;
-    let rpcEndpoints = ['https://rpc.hellomoon.io/cfd5910f-fb7d-4489-9b32-f97193eceefd','https://solana-api.syndica.io/access-token/WPoEqWQ2auQQY1zHRNGJyRBkvfOLqw58FqYucdYtmy8q9Z84MBWwqtfVf8jKhcFh/rpc'];
+    let rpcEndpoints = ['https://solana-mainnet.g.alchemy.com/v2/D3Tx6_4zfGg-YgjQkyO38','https://mainnet.helius-rpc.com/?api-key=7848a30b-3c4f-4777-a802-93aaa5d844e9/rpc'];
     const priorityFee = 1; // Priority Fee added to each transaction in Lamports. Set to 0 (zero) to disable priority fees. 1 Lamport = 0.000000001 SOL
 
     const connectionProxy = {
@@ -3645,3 +3645,4 @@
 
 
 })();
+
